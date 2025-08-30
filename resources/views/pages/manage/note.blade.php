ed69c3a6d250e3800f1e06835cdd171aa45e82aa
unknown <salaudeenramadan8@gmail.com>
2025-08-30 19:55:58 +0100

    'error-fix'

--- resources/views/pages/manage/note.blade.php
+++ resources/views/pages/manage/note.blade.php
@@ -97,7 +97,7 @@
             <!-- Notes Grid -->
                 @forelse($notes as $note)
                     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                        @foreach($notes['data'] as $note)
+                        @foreach($notes as $note)
                             <x-note-card
                                 title="{{ $note->title }}"
                                 body="{!! $note->body !!}"
